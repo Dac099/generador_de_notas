7281197f93df080c7a45de25ce0cb73ee44258a9
Dac099 <davidcejazapata@gmail.com>
2022-08-15 11:11:46 -0500

    Terminado, subir archivos

--- index.js
+++ index.js
@@ -11,19 +11,27 @@ const messages = {
   3 : 'Ya son 29 meses juntos, ratita blanca. A lo largo de este tiempo te has ganado diversos apodos que hacen justicia a lo hermosa que estás y a al inmenso amor que tengo por ti.<br>Cada que sostengo tu mano me siento muy seguro y lleno de amor, me has hecho saber que tengo mucho amor para dar y por supuesto que quiero darte todo el amor que tengo.<br>Todo este tiempo me he sentido muy agradecido de haber trabajado en San Cafecito, ya que ahi te conocí y fue el inicio de la relación más hermosa que he tenido.<br>Todos los días me pregunto si me amas, porque sin duda yo te amo como si fueras la utlima coca del desierto, y ya no puedo imaginar un futuro si no estás ahí.<br>Así que sin duda puedo decir que eres mi ratita, mi cabeza de balón, mi cachorro, mi ojos de becerro, mi chipilona, mi cielito, mi amorcito, mi bb que solo come 5 cosas, eres el amor de vida.'
 }
 
-function* control(index){
-  while (index < 4) {
-    yield index;
-    index++;
-  }
-}
-
-let msgControl = control(1);
+let control = 1;
 
 mainLayout.btn.addEventListener('click', () => {
-  if(!msgControl.next().done){
-    mainLayout.text.innerHTML = messages[msgControl.next().value];
+  mainLayout.text.classList.remove('pink');
+  mainLayout.text.classList.remove('purple');
+
+  if(control < 4){
+    if(control === 2){
+      mainLayout.text.classList.remove('pink');
+      mainLayout.text.classList.add('pink');
+    }
+
+    if(control === 3){
+      mainLayout.text.classList.remove('pink');
+      mainLayout.text.classList.add('purple');
+    }
+
+    mainLayout.text.innerHTML = messages[control];
+    control++;
   }else{
-    msgControl = control(1);
+    control = 1;
+    mainLayout.text.innerHTML = 'Presiona el botón para ver la nota del mes C:';
   }
 });
